04deea218fdc7146f642dfbe777a1dd73ca46cbb
Komiroko <komiriko@pm.me>
2025-08-27 11:55:13 +0200

    refactor(🧹): [Refactor caching mechanism for improved readability] ## Refactoring - Simplified state management for cache routes. - Removed unnecessary variables and streamlined cache retrieval logic. - Enhanced clarity in cache handling by restructuring code flow.
    ## Description
    This commit refactors the caching mechanism to improve code readability and maintainability. The state management for cached routes has been simplified, and unnecessary variables have been removed. The cache retrieval logic has been streamlined to enhance clarity in how cached responses are handled.

--- source/cache.ts
+++ source/cache.ts
@@ -3,30 +3,41 @@ import { Elysia } from 'elysia';
 import { MemoryStore } from '@nowarajs/kv-store/memory';
 import type { CacheOptions } from './types/cacheOptions';
 import { generateCacheKey } from './utils/generateCacheKey';
+import type { CacheItem } from './types/cacheItem';
 
 export const cache = ({
 	defaultTtl = 60,
 	prefix = '',
 	store = ':memory:'
 }: CacheOptions = {}) => new Elysia()
-	.state(
-		'kvStore',
-		store === ':memory:'
+	.state({
+		kvStore: store === ':memory:'
 			? new MemoryStore()
 			: store
-	)
-	.state('_cacheKey', '')
+	})
+	.state({
+		_cachedRoutes: new Set<string>()
+	})
 	.onRequest(async ({ request, store, set }) => {
-		const cacheKey = await generateCacheKey(request.clone());
-		const cachedResponse = await store.kvStore.get(`${prefix}${cacheKey}`);
-
-		store._cacheKey = cacheKey;
-
-		if (cachedResponse) {
-			set.headers['Cache-Control'] = `max-age=${defaultTtl}, public`;
-			set.headers['X-Cache'] = 'HIT';
-			set.headers['ETag'] = `"${prefix}${cacheKey}"`;
-			return cachedResponse;
+		const sanitizeUrl = (new URL(request.url)).pathname;
+		if (store._cachedRoutes.has(`${request.method}:${sanitizeUrl}`)) {
+			const cacheKey = await generateCacheKey(request.clone());
+			const cachedData = await store.kvStore.get(`${prefix}${cacheKey}`);
+			if (
+				cachedData
+				&& typeof cachedData === 'object'
+				&& 'response' in cachedData
+				&& 'metadata' in cachedData
+			) {
+				const { response, metadata } = cachedData as CacheItem;
+				set.headers['cache-control'] = `max-age=${defaultTtl}, public`;
+				set.headers['x-cache'] = 'HIT';
+				set.headers['etag'] = `"${prefix}${cacheKey}"`;
+				set.headers['expires'] = new Date(Date.now() + (defaultTtl * 1000)).toUTCString();
+				set.headers['last-modified'] = metadata.createdAt;
+				return response;
+			}
+			set.headers['x-cache'] = 'MISS';
 		}
 		return void 0;
 	})
@@ -36,16 +47,27 @@ export const cache = ({
 				? enable
 				: (enable ? defaultTtl : 0);
 			return {
-				async afterHandle({ set, response, store }) {
-					const cacheKey = store._cacheKey;
+				async afterHandle({ set, response, store, request }) {
+					const sanitizeUrl = (new URL(request.url)).pathname;
+					if (!store._cachedRoutes.has(`${request.method}:${sanitizeUrl}`))
+						store._cachedRoutes.add(`${request.method}:${sanitizeUrl}`);
+
+					const cacheKey = await generateCacheKey(request.clone());
 					const now = new Date();
+					set.headers['cache-control'] = `max-age=${ttl}, public`;
+					set.headers['etag'] = `"${prefix}${cacheKey}"`;
+					set.headers['last-modified'] = now.toUTCString();
+					set.headers['expires'] = new Date(now.getTime() + (ttl * 1000)).toUTCString();
 
-					set.headers['Cache-Control'] = `max-age=${ttl}, public`;
-					set.headers['ETag'] = `"${prefix}${cacheKey}"`;
-					set.headers['Last-Modified'] = now.toUTCString();
-					set.headers['Expires'] = new Date(now.getTime() + (ttl * 1000)).toUTCString();
+					const cacheData = {
+						response,
+						metadata: {
+							createdAt: now.toUTCString(),
+							ttl
+						}
+					};
 
-					await store.kvStore.set(`${prefix}${cacheKey}`, response, ttl);
+					await store.kvStore.set(`${prefix}${cacheKey}`, cacheData, ttl);
 				}
 			};
 		}
